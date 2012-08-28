d63186ad2d27af34c7b3c424646903a5dcb7ae42
Christophe Hamerling <christophe.hamerling@gmail.com>
2012-08-28 17:37:36 +0200

    create migrator

---
+++ governance-migrator/src/main/java/org/ow2/play/governance/migrator/SubscriptionRegistryV1ToV1.java
@@ -0,0 +1,69 @@
+/**
+ * 
+ */
+package org.ow2.play.governance.migrator;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import org.ow2.play.governance.api.GovernanceExeption;
+import org.ow2.play.governance.api.SubscriptionRegistry;
+import org.ow2.play.governance.api.bean.Subscription;
+import org.petalslink.dsb.cxf.CXFHelper;
+
+/**
+ * Migrate data from V1 registry to V1 registry
+ * 
+ * @author chamerling
+ * 
+ */
+public class SubscriptionRegistryV1ToV1 {
+
+	private static final Logger logger = Logger
+			.getLogger(SubscriptionRegistryV1ToV1.class.getName());
+
+	/**
+	 * Return the list of subscriptions which have been migrated from source to
+	 * destination
+	 * 
+	 * @param source
+	 * @param destination
+	 * @return
+	 */
+	public static List<Subscription> migrate(String source, String destination)
+			throws GovernanceExeption {
+
+		if (source == null || destination == null) {
+			throw new GovernanceExeption(
+					"Source and destination must be filled");
+		}
+
+		List<Subscription> result = new ArrayList<Subscription>();
+
+		SubscriptionRegistry sourceRegistry = CXFHelper.getClientFromFinalURL(
+				source, SubscriptionRegistry.class);
+
+		SubscriptionRegistry destinationRegistry = CXFHelper
+				.getClientFromFinalURL(destination, SubscriptionRegistry.class);
+
+		List<Subscription> sources = sourceRegistry.getSubscriptions();
+		if (sources != null) {
+			for (Subscription subscription : sources) {
+				try {
+					destinationRegistry.addSubscription(subscription);
+					result.add(subscription);
+					if (logger.isLoggable(Level.INFO)) {
+						logger.info("Added subscription = " + subscription);
+					}
+				} catch (Exception e) {
+					logger.warning("Can not add the subscription to destination : "
+							+ subscription);
+				}
+			}
+		}
+		return result;
+	}
+
+}
---
+++ governance-migrator/src/test/java/org/ow2/play/governance/migrator/SubscriptionRegistryV1ToV1Test.java
@@ -0,0 +1,76 @@
+/**
+ * 
+ */
+package org.ow2.play.governance.migrator;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import junit.framework.TestCase;
+
+import org.ow2.play.governance.api.SubscriptionRegistry;
+import org.ow2.play.governance.api.bean.Subscription;
+import org.ow2.play.governance.api.bean.Topic;
+import org.ow2.play.governance.service.InMemorySubscriptionRegistryService;
+import org.petalslink.dsb.commons.service.api.Service;
+import org.petalslink.dsb.cxf.CXFHelper;
+
+/**
+ * @author chamerling
+ * 
+ */
+public class SubscriptionRegistryV1ToV1Test extends TestCase {
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see junit.framework.TestCase#setUp()
+	 */
+	protected void setUp() throws Exception {
+		super.setUp();
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see junit.framework.TestCase#tearDown()
+	 */
+	protected void tearDown() throws Exception {
+		super.tearDown();
+	}
+
+	public void testMigrate() throws Exception {
+		String source = "http://localhost:8493/foo/bar/Source";
+		String destination = "http://localhost:8493/foo/bar/Destination";
+
+		InMemorySubscriptionRegistryService sourceRegistry = new InMemorySubscriptionRegistryService();
+		InMemorySubscriptionRegistryService destRegistry = new InMemorySubscriptionRegistryService();
+
+		Service sourceService = CXFHelper.getServiceFromFinalURL(source,
+				SubscriptionRegistry.class, sourceRegistry);
+		Service destService = CXFHelper.getServiceFromFinalURL(destination,
+				SubscriptionRegistry.class, destRegistry);
+
+		List<Subscription> subscriptions = new ArrayList<Subscription>();
+		for (int i = 0; i < 10; i++) {
+			Topic t = new Topic();
+			t.setName("" + i);
+			t.setNs("http://foo/" + i);
+			t.setPrefix("pre");
+			Subscription s = new Subscription("" + i, "subscriber-" + i,
+					"provider-" + i, t, System.currentTimeMillis());
+			subscriptions.add(s);
+			sourceRegistry.addSubscription(s);
+		}
+
+		sourceService.start();
+		destService.start();
+
+		List<Subscription> result = SubscriptionRegistryV1ToV1.migrate(source,
+				destination);
+		assertNotNull(result);
+		assertEquals(subscriptions.size(), result.size());
+
+	}
+
+}
