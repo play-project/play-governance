b92649913829b240c0318e64f73394e8f57cd939
Christophe Hamerling <christophe.hamerling@gmail.com>
2012-08-29 14:19:42 +0200

    add status in subscription

--- governance-api/src/main/java/org/ow2/play/governance/api/bean/Subscription.java
+++ governance-api/src/main/java/org/ow2/play/governance/api/bean/Subscription.java
@@ -35,6 +35,7 @@ public class Subscription {
 	String provider;
 	Topic topic;
 	long date = -1L;
+	String status;
 
 	/**
 	 * 
@@ -135,16 +136,73 @@ public class Subscription {
 		return date;
 	}
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see java.lang.Object#toString()
-	 */
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
 	@Override
 	public String toString() {
 		return "Subscription [id=" + id + ", subscriber=" + subscriber
 				+ ", provider=" + provider + ", topic=" + topic + ", date="
-				+ date + "]";
+				+ date + ", status=" + status + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + (int) (date ^ (date >>> 32));
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result
+				+ ((provider == null) ? 0 : provider.hashCode());
+		result = prime * result + ((status == null) ? 0 : status.hashCode());
+		result = prime * result
+				+ ((subscriber == null) ? 0 : subscriber.hashCode());
+		result = prime * result + ((topic == null) ? 0 : topic.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Subscription other = (Subscription) obj;
+		if (date != other.date)
+			return false;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (provider == null) {
+			if (other.provider != null)
+				return false;
+		} else if (!provider.equals(other.provider))
+			return false;
+		if (status == null) {
+			if (other.status != null)
+				return false;
+		} else if (!status.equals(other.status))
+			return false;
+		if (subscriber == null) {
+			if (other.subscriber != null)
+				return false;
+		} else if (!subscriber.equals(other.subscriber))
+			return false;
+		if (topic == null) {
+			if (other.topic != null)
+				return false;
+		} else if (!topic.equals(other.topic))
+			return false;
+		return true;
 	}
 
 }
