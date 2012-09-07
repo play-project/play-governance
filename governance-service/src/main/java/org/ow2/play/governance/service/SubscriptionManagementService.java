535342c74e25ff457596c199b31a5b1e660ad02f
Christophe Hamerling <christophe.hamerling@gmail.com>
2012-09-07 11:23:30 +0200

    add management service

--- governance-service/src/main/java/org/ow2/play/governance/service/SubscriptionManagementService.java
+++ governance-service/src/main/java/org/ow2/play/governance/service/SubscriptionManagementService.java
@@ -5,6 +5,7 @@ package org.ow2.play.governance.service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Logger;
 
 import javax.jws.WebMethod;
 
@@ -15,21 +16,46 @@ import org.ow2.play.governance.api.bean.Subscription;
 
 /**
  * TODO
+ * 
  * @author chamerling
  * 
  */
 public class SubscriptionManagementService implements SubscriptionManagement {
-	
-	//private SubscriptionService SubscriptionService;
+
+	private static Logger logger = Logger
+			.getLogger(SubscriptionManagementService.class.getName());
+
+	private SubscriptionService subscriptionService;
 
 	@Override
 	@WebMethod
 	public List<Subscription> subscribe(List<Subscription> subscriptions)
 			throws GovernanceExeption {
+		logger.fine("Create subscriptions from list");
+		
 		if (subscriptions == null) {
 			throw new GovernanceExeption("Can not create null subscription");
 		}
-		throw new GovernanceExeption("subscribe :: Not implemented");
+
+		List<Subscription> result = new ArrayList<Subscription>();
+
+		for (Subscription subscription : subscriptions) {
+			Subscription s = null;
+			logger.fine("Create subscription for " + subscription);
+			
+			try {
+				s = this.subscriptionService.subscribe(subscription);
+				if (s != null) {
+					s.setStatus("created");
+					result.add(s);
+				}
+			} catch (Exception e) {
+				subscription.setStatus(e.getMessage());
+				result.add(subscription);
+			}
+		}
+
+		return result;
 	}
 
 	@Override
@@ -51,6 +77,14 @@ public class SubscriptionManagementService implements SubscriptionManagement {
 	public List<Subscription> replay(List<Subscription> subscriptions)
 			throws GovernanceExeption {
 		throw new GovernanceExeption("replay :: Not implemented");
+	}
+
+	public void setSubscriptionService(SubscriptionService subscriptionService) {
+		this.subscriptionService = subscriptionService;
+	}
+
+	public SubscriptionService getSubscriptionService() {
+		return this.subscriptionService;
 	}
 
 }
