4f86825668d3688370c3c7eb3323f6df0305ba4f
Christophe Hamerling <christophe.hamerling@gmail.com>
2013-03-20 16:00:21 +0100

    Only get topics where user has access ie define in permission and belongs to group

--- governance-platform-userservice/src/main/java/org/ow2/play/governance/platform/user/service/rest/TopicService.java
+++ governance-platform-userservice/src/main/java/org/ow2/play/governance/platform/user/service/rest/TopicService.java
@@ -20,8 +20,9 @@
 package org.ow2.play.governance.platform.user.service.rest;
 
 import static org.ow2.play.governance.platform.user.api.rest.helpers.Response.error;
-import static org.ow2.play.governance.platform.user.api.rest.helpers.Response.ok;
 import static org.ow2.play.governance.platform.user.api.rest.helpers.Response.notFound;
+import static org.ow2.play.governance.platform.user.api.rest.helpers.Response.ok;
+import static org.ow2.play.governance.platform.user.api.rest.helpers.Response.unauthorized;
 
 import java.net.URI;
 import java.util.Collection;
@@ -35,8 +36,10 @@ import org.apache.cxf.jaxrs.ext.MessageContext;
 import org.ow2.play.governance.api.EventGovernance;
 import org.ow2.play.governance.api.GovernanceExeption;
 import org.ow2.play.governance.api.bean.Topic;
+import org.ow2.play.governance.user.api.bean.User;
 
 import com.google.common.base.Function;
+import com.google.common.base.Predicate;
 import com.google.common.collect.Collections2;
 
 /**
@@ -54,22 +57,30 @@ public class TopicService extends AbstractService implements
 	@Override
 	public Response topics() {
 
-		// get the user from the context and its topics
-		// topics from public streams are also available
-		// TODO
-
 		List<Topic> topics = null;
 		try {
 			topics = eventGovernance.getTopics();
 		} catch (GovernanceExeption e) {
-			org.ow2.play.governance.platform.user.api.rest.bean.Error error = new org.ow2.play.governance.platform.user.api.rest.bean.Error();
-			error.message = "Can not get topics";
-			return Response.serverError().entity(error).build();
+			e.printStackTrace();
+			return error(Status.INTERNAL_SERVER_ERROR, "Can not get topics");
 		}
+		
+		// filter the topics based on the user permissions
+		// TODO : Topics per user can be found before instead of calling N times the permission checker...
+		final User user = getUser(mc);
+
+		Collection<Topic> filtered = Collections2.filter(topics,
+				new Predicate<Topic>() {
+					public boolean apply(Topic topic) {
+						// get the topic as resource to check permissions since groups are resources in the platform...
+						String resource = org.ow2.play.governance.api.helpers.ResourceHelper.get(topic);
+						return permissionChecker.checkResource(user.login, resource);
+					}
+				});
 
 		Collection<org.ow2.play.governance.platform.user.api.rest.bean.Topic> out = Collections2
 				.transform(
-						topics,
+						filtered,
 						new Function<Topic, org.ow2.play.governance.platform.user.api.rest.bean.Topic>() {
 							public org.ow2.play.governance.platform.user.api.rest.bean.Topic apply(
 									Topic input) {
@@ -103,13 +114,19 @@ public class TopicService extends AbstractService implements
 		
 		// get the first one...
 		Topic input = topics.get(0);
-		org.ow2.play.governance.platform.user.api.rest.bean.Topic topic = new org.ow2.play.governance.platform.user.api.rest.bean.Topic();
-		topic.name = input.getName();
-		topic.ns = input.getNs();
-		topic.prefix = input.getPrefix();
-		topic.resourceUrl = getResourceURI(input);
 		
-		return ok(topic);
+		// check access
+		final User user = getUser(mc);
+		if (permissionChecker.checkResource(user.login, org.ow2.play.governance.api.helpers.ResourceHelper.get(input))) {
+			org.ow2.play.governance.platform.user.api.rest.bean.Topic topic = new org.ow2.play.governance.platform.user.api.rest.bean.Topic();
+			topic.name = input.getName();
+			topic.ns = input.getNs();
+			topic.prefix = input.getPrefix();
+			topic.resourceUrl = getResourceURI(input);
+			return ok(topic);
+		} else {
+			return unauthorized();
+		}
 	}
 
 	protected String getResourceURI(Topic topic) {
