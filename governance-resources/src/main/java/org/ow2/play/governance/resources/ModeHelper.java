916f8cae16e36bb5cdde3722884b6c0d8be9ac3d
Christophe Hamerling <christophe.hamerling@gmail.com>
2013-07-09 15:39:21 +0200

    Better mode handling

--- governance-resources/src/main/java/org/ow2/play/governance/resources/ModeHelper.java
+++ governance-resources/src/main/java/org/ow2/play/governance/resources/ModeHelper.java
@@ -1,16 +1,47 @@
 package org.ow2.play.governance.resources;
 
+import com.google.common.collect.Maps;
+
+import java.util.Map;
+
 /**
  * @author Christophe Hamerling - chamerling@linagora.com
  */
 public class ModeHelper {
 
     public static final String ACL_PREFIX = "http://www.w3.org/ns/auth/acl";
+    public static final String NOTIFY = "http://docs.oasis-open.org/wsn/b-2/Notify";
+    public static final String SUBSCRIBE = "http://docs.oasis-open.org/wsn/b-2/Subscribe";
+    public static final String READ = "http://www.w3.org/ns/auth/acl#Read";
+    public static final String WRITE = "http://www.w3.org/ns/auth/acl#Write";
 
+    public static Map<String, String> modes = Maps.newHashMap();
+
+    static {
+        modes.put("notify", NOTIFY);
+        modes.put("subscribe", SUBSCRIBE);
+        modes.put("read", READ);
+        modes.put("write", WRITE);
+    }
+
+    /**
+     * Return a full URI for the mode based on the modes cache.
+     *
+     * @param mode the mode to get URI from
+     *
+     * @return
+     */
     public static String getFullURI(String mode) {
         if (mode.startsWith(ACL_PREFIX)) {
             return mode;
         }
+
+        // lookup in the cache if we can find a mode (in lowercase)
+        if (null != modes.get(mode.toLowerCase())) {
+            return modes.get(mode.toLowerCase());
+        }
+
+        // not found in cache, build an URI and send it back
         return ACL_PREFIX + "#" + mode;
     }
 }
--- governance-resources/src/test/java/org/ow2/play/governance/resources/ModeHelperTest.java
+++ governance-resources/src/test/java/org/ow2/play/governance/resources/ModeHelperTest.java
@@ -19,4 +19,28 @@ public class ModeHelperTest {
         String mode = "http://www.w3.org/ns/auth/acl#bar";
         Assert.assertEquals(mode, ModeHelper.getFullURI(mode));
     }
+
+    @Test
+    public void testGetReadMode() {
+        Assert.assertEquals("http://www.w3.org/ns/auth/acl#Read", ModeHelper.getFullURI("read"));
+        Assert.assertEquals("http://www.w3.org/ns/auth/acl#Read", ModeHelper.getFullURI("Read"));
+    }
+
+    @Test
+    public void testGetWriteMode() {
+        Assert.assertEquals("http://www.w3.org/ns/auth/acl#Write", ModeHelper.getFullURI("write"));
+        Assert.assertEquals("http://www.w3.org/ns/auth/acl#Write", ModeHelper.getFullURI("Write"));
+    }
+
+    @Test
+    public void testGetSubscribeMode() {
+        Assert.assertEquals("http://docs.oasis-open.org/wsn/b-2/Subscribe", ModeHelper.getFullURI("subscribe"));
+        Assert.assertEquals("http://docs.oasis-open.org/wsn/b-2/Subscribe", ModeHelper.getFullURI("Subscribe"));
+    }
+
+    @Test
+    public void testNotifyMode() {
+        Assert.assertEquals("http://docs.oasis-open.org/wsn/b-2/Notify", ModeHelper.getFullURI("notify"));
+        Assert.assertEquals("http://docs.oasis-open.org/wsn/b-2/Notify", ModeHelper.getFullURI("Notify"));
+    }
 }
