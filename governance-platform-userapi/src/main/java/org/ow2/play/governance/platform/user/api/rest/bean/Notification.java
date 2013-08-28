9decba4641cb5dd38875b4bda92fac5f125c363f
stuehmer <roland.stuehmer@fzi.de>
2013-08-28 11:58:36 +0200

    Add optional extensions to the Notification bean

--- governance-platform-userapi/src/main/java/org/ow2/play/governance/platform/user/api/rest/bean/Notification.java
+++ governance-platform-userapi/src/main/java/org/ow2/play/governance/platform/user/api/rest/bean/Notification.java
@@ -14,7 +14,7 @@
  *
  * You should have received a copy of the GNU Lesser General Public
  * License along with this library; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA 
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
  *
  */
 package org.ow2.play.governance.platform.user.api.rest.bean;
@@ -30,17 +30,32 @@ import javax.xml.bind.annotation.XmlRootElement;
 public class Notification {
 	
 	/**
-	 * Target resource (stream/topic/whatever)
+	 * Target resource (stream including {@code #stream} suffix)
 	 */
-	@XmlElement(name = "resource")
+	@XmlElement(name = "resource", required = true)
 	public String resource;
 	
 	/**
 	 * Payload of the notification
 	 * 
 	 */
-	@XmlElement(name = "message")
+	@XmlElement(name = "message", required = true)
 	public String message;
+	
+	/**
+	 * Optional RDF mediatype of the notification
+	 * 
+	 */
+	@XmlElement(name = "messageMediatype")
+	public String messageMediatype;
+	
+	/**
+	 * Optional graph name of the notification e.g. if mediatype does not
+	 * support quadruples containing a graph name
+	 * 
+	 */
+	@XmlElement(name = "messageGraph")
+	public String messageGraph;
 	
 	/**
 	 * 
