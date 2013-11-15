0e699c9593edcd79056f4f5c39fe2954764d9f63
stuehmer <roland.stuehmer@fzi.de>
2013-11-15 12:25:04 +0100

    Remove empty (useless) path annotation
    ... avoiding a warning when using with Jersey 2.x:
    The following warnings have been detected: WARNING: The (sub)resource
    method notify in [...] contains empty path annotation.

--- governance-platform-userapi/src/main/java/org/ow2/play/governance/platform/user/api/rest/PublishService.java
+++ governance-platform-userapi/src/main/java/org/ow2/play/governance/platform/user/api/rest/PublishService.java
@@ -14,7 +14,7 @@
  *
  * You should have received a copy of the GNU Lesser General Public
  * License along with this library; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA 
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
  *
  */
 package org.ow2.play.governance.platform.user.api.rest;
@@ -49,7 +49,6 @@ public interface PublishService {
 	 * @return HTTP Status 200 if data has been published
 	 */
 	@POST
-	@Path("/")
 	@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
 	@Produces(MediaType.APPLICATION_JSON)
 	Response notify(@FormParam("resource") String resource,
